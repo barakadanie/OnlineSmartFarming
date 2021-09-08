56569cb08b7305bfa4ee2387daefd21f40917a61
barakadanie <74895402+barakadanie@users.noreply.github.com>
2021-09-08 17:36:31 +0300

    commit

--- admin/login.php
+++ admin/login.php
@@ -9,14 +9,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 //import bootstrap styling 
 ?>
-<!doctype html>
+<!-- <!doctype html>
 <html class="no-js" lang="">
-   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-   <head>
+<head>
+<title>Admin Login Page</title>
+      <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
       <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>Admin Login Page</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge",chrome="1">
+      <meta name="HandheldFriendly" content="true">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0 ,maximum-scale=1,user-scalable=no">
       <link rel="stylesheet" href="assets/css/normalize.css">
       <link rel="stylesheet" href="assets/css/bootstrap.min.css">
       <link rel="stylesheet" href="assets/css/font-awesome.min.css">
@@ -28,35 +29,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       <link rel="stylesheet" href="assets/css/back.css">
       <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
    </head>
-   <style>
-      /* insert a background image behind the login interface */
-      body{
-         background-image: url(img/home.jpg);
-      }
-      /* styles up the login inter to the centre of the paage*/
-      .inputs
-      {
-         padding-top: 90px;
-         margin-left: 600px;
-         width: 560px;
-         height: 90px;
-
-      }
-      .me
-      {
-         padding-top: 10px;
-         margin-left:-55%;
-         align-items: center;
-      }
-        </style>
-   <body class="bg-dark" background=src="/..img/27315.jpg" >
+  <body class="bg-dark">
       <div class="sufee-login d-flex align-content-center flex-wrap">
       <div class="inputs">
       <div class="me">
          <div class="container">
             <div class="login-content">
-               <div class="login-form mt-150">
-                  <form action="login.php" method="post" style ="background-image: url('/img/27315.jpg');">
+               <div class="login-form mt-150" style ="height:60%;width:200%;margin-top:50%;margin-left: 120%;";>
+                  <form action="login.php" method="post" style ="width:100%;margin-top: -13px;">
                           <?php
                            if (isset($loginchk)) {
                               echo $loginchk;
@@ -84,4 +64,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       <script src="assets/js/plugins.js" type="text/javascript"></script>
       <script src="assets/js/main.js" type="text/javascript"></script>
    </body>
+</html> -->
+
+<html>
+<head>
+ 
+   <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+  <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ <link rel="stylesheet" href="css/">
+ <link rel="stylesheet" href="assets/css/login_style.css">
+</head>
+ 
+<body>
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!-- Login form creation starts-->
+  <section class="container-fluid">
+    <!-- row and justify-content-center class is used to place the form in center -->
+    <section class="row justify-content-center">
+      <section class="col-12 col-sm-6 col-md-4">
+        <form class="form-container">
+        <div class="form-group">
+          <h4 class="text-center font-weight-bold"> Login </h4>
+          <label for="InputEmail1">Email Address</label>
+           <input type="email" class="form-control" id="InputEmail1" aria-describeby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="InputPassword1">Password</label>
+          <input type="password" class="form-control" id="InputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Submit</button>
+        <div class="form-footer">
+          <p> Don't have an account? <a href="#">Sign Up</a></p>
+          
+        </div>
+        </form>
+      </section>
+    </section>
+  </section>
+<!-- Login form creation ends -->
+</body>
 </html>
